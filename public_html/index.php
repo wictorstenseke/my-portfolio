250ddb899dcd8c3ec8126bba00f02d19335d5515
wictorstenseke <wictor.johansson@gmail.com>
2016-11-19 18:38:53 +0100

    finished up to portfolio

---
+++ public_html/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Välkommen att kontakta mig via LinkedIn eller Facebook!">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Wictor Stenseke | UX &amp; Web Designer</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,700,900" rel="stylesheet">
+
+  <script src="https://use.fontawesome.com/5cafce8111.js"></script>
+</head>
+<body>
+
+  <section class="header">
+    <div class="hero-image">
+      <div class="logo">
+        <p>Wictor</p>
+        <p>Stenseke</p>
+      </div>
+      <div class="pure-g">
+        <div class="pure-u-md-1-2 pure-u-1">
+          <h1>UX Design | Service Design</h1>
+        </div>
+      </div>
+      <i class="fa fa-angle-down fa-3x" aria-hidden="true"></i>
+    </div>
+  </section>
+
+  <section class="about">
+    <div class="section-wrapper">
+      <img class="avatar" src="http://placehold.it/180x180">
+      <p class="biography">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
+      <p class="biography">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+      <div class="social-icons">
+        <!-- Icons made by Freepik from www.flaticon.com is licensed by CC 3.0 BY -->
+        <a href="https://se.linkedin.com/in/wictorstenseke" target="_blank" class="linkedin">
+          <svg class="linkedin-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             viewBox="0 0 382 382" style="enable-background:new 0 0 382 382;" xml:space="preserve">
+            <path class="linkedin-icon-color" d="M347.445,0H34.555C15.471,0,0,15.471,0,34.555v312.889C0,366.529,15.471,382,34.555,382h312.889
+              C366.529,382,382,366.529,382,347.444V34.555C382,15.471,366.529,0,347.445,0z M118.207,329.844c0,5.554-4.502,10.056-10.056,10.056
+              H65.345c-5.554,0-10.056-4.502-10.056-10.056V150.403c0-5.554,4.502-10.056,10.056-10.056h42.806
+              c5.554,0,10.056,4.502,10.056,10.056V329.844z M86.748,123.432c-22.459,0-40.666-18.207-40.666-40.666S64.289,42.1,86.748,42.1
+              s40.666,18.207,40.666,40.666S109.208,123.432,86.748,123.432z M341.91,330.654c0,5.106-4.14,9.246-9.246,9.246H286.73
+              c-5.106,0-9.246-4.14-9.246-9.246v-84.168c0-12.556,3.683-55.021-32.813-55.021c-28.309,0-34.051,29.066-35.204,42.11v97.079
+              c0,5.106-4.139,9.246-9.246,9.246h-44.426c-5.106,0-9.246-4.14-9.246-9.246V149.593c0-5.106,4.14-9.246,9.246-9.246h44.426
+              c5.106,0,9.246,4.14,9.246,9.246v15.655c10.497-15.753,26.097-27.912,59.312-27.912c73.552,0,73.131,68.716,73.131,106.472
+              L341.91,330.654L341.91,330.654z"/>
+          <g>
+          </g>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <div class="hr"></div>
+
+  <section class="portfolio">
+    <div class="portfolio-wrapper">
+
+      <div class="pure-g">
+        <div class="pure-u-md-1-2 pure-u-1 project-info">
+          <div class="info-content">
+            <h2>Inspelning</h2>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly.</p>
+          </div>
+        </div>
+        <div class="pure-u-md-1-2 pure-u-1 project-image">
+          <img src="img/hogelid1.png" alt="Bild på webbplats för bokning på Högelids Tennisklubb.">
+        </div>
+      </div>
+
+      <div class="pure-g">
+        <div class="pure-u-md-1-2 pure-u-1 project-image">
+          <img src="img/noizier3.png" alt="Bild på webbplats för en ljudtekniker.">
+        </div>
+        <div class="pure-u-md-1-2 pure-u-1 project-info">
+          <div class="info-content">
+            <h2>Inspelning</h2>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="pure-g">
+        <div class="pure-u-md-1-2 pure-u-1 project-info">
+          <div class="info-content">
+            <h2>Inspelning</h2>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            <p>He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly.</p>
+          </div>
+        </div>
+        <div class="pure-u-md-1-2 pure-u-1 project-image">
+          <img src="img/blueprint2.png" alt="Bild på en Service Blueprint som används inom Service Design.">
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+</body>
+</html>
