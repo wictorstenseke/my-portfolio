28b735fddf662823f13c5cd3c6475136b726e544
wictorstenseke <wictor.johansson@gmail.com>
2017-03-01 23:46:49 +0100

    added favicon and facebook-metas

--- includes/head.php
+++ includes/head.php
@@ -1,18 +1,21 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<meta property="og:url" content="http://www.wictorstenseke.se" />
-<meta property="og:title" content="UX Designer | Wictor Stenseke" />
-<meta property="og:description" content="UX Designer | Min portfolio över projekt jag skapat eller deltagit inom. Välkommen att kontakta mig via Linkedin eller Facebook!" />
-<meta property="og:image:url" content="http://www.wictorstenseke.se/img/fb-share.png" />
+<meta property="og:image:width" content="1614">
+<meta property="og:image:height" content="845">
+<meta property="og:image" content="http://wictorstenseke.se/img/og-image.jpg">
+<meta property="og:title" content="UX DESIGNER | Wictor Stenseke" />
+<meta property="og:description" content="Min portfolio med projekt, hemsidor och tjänster jag skapat eller deltagit inom. Välkommen att kontakta mig via Linkedin eller Facebook!" />
+<meta property="og:url" content="wictorstenseke.se">
 <meta property="og:locale" content="sv_SE" />
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2">
-<link rel="icon" type="image/png" href="/favicon-32x32.png?v=2" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-16x16.png?v=2" sizes="16x16">
-<link rel="manifest" href="/manifest.json?v=2">
-<link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#5bbad5">
-<link rel="shortcut icon" href="/favicon.ico?v=2">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=3" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=3" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=3">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=3" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon.ico?v=3">
 <meta name="theme-color" content="#ffffff">
 
 <link rel="stylesheet" href="/css/all.css">
