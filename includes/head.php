8c6ac2a4c3321a3720cac1cf1f43a10bb8a47f45
wictorstenseke <wictor.johansson@gmail.com>
2017-03-01 10:35:44 +0100

    update and refresh favicon

--- includes/head.php
+++ includes/head.php
@@ -7,11 +7,12 @@
 <meta property="og:image:url" content="http://www.wictorstenseke.se/img/fb-share.png" />
 <meta property="og:locale" content="sv_SE" />
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=2" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=2" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=2">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon.ico?v=2">
 <meta name="theme-color" content="#ffffff">
 
 <link rel="stylesheet" href="/css/main.css">
