0d8507187a9daf5e145c116ec0fdd12f407877dc
wictorstenseke <wictor.johansson@gmail.com>
2017-03-09 16:48:04 +0100

    inleed server-strul

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
 
 <meta property="og:image:width" content="1614">
 <meta property="og:image:height" content="845">
-<meta property="og:image" content="/img/og-image.jpg">
+<meta property="og:image" content="http://wictorstenseke.se/img/og-image.jpg">
 <meta property="og:title" content="UX DESIGNER | Wictor Stenseke" />
 <meta property="og:description" content="Min portfolio med projekt, hemsidor och tjänster jag skapat eller deltagit inom. Välkommen att kontakta mig via Linkedin eller Facebook!" />
 <meta property="og:url" content="http://wictorstenseke.se">
