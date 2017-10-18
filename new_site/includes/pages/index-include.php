790107d64aa483a40ffeab88a420f3f4987b12c6
Wictor Stenseke <wictor.stenseke@knowit.se>
2017-10-18 13:18:45 +0200

    changed new sites structure

---
+++ new_site/includes/pages/index-include.php
@@ -0,0 +1,23 @@
+    <!-- change the header here -->
+    <div class="container-fluid">
+        <div class="row banner-header">
+            <div class="col-xs-12 text-center">
+                <div class="my-profile">
+                    <img class="profile-avatar" src="/img/avatar.jpg">
+                    <h3>Wictor Stenseke</h3>
+                    <p>UX Designer at Knowit Solutions</p>
+                    <p class="bold-text bio-end"><img class="pin" src="/img/pin.svg">Göteborg, Sverige</p>
+                    <div class="col-sm-4  col-sm-offset-2 button-set">
+                        <a class="btn cta-button-mail" role="button" href="mailto:wictorstenseke@gmail.com">Maila mig&nbsp;&nbsp;&nbsp;<i class="fa fa-paper-plane" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="col-sm-4">
+                        <a class="btn cta-button-linkedin" role="button" href="https://www.linkedin.com/in/wictorstenseke/" target="_blank">LinkedIn&nbsp;&nbsp;&nbsp;<i class="fa fa-linkedin-square " aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-xs-12 footer-ws text-center">
+                        <!-- <div class="hr"></div> -->
+                        <p class="copyright">© 2017 | wictorstenseke.se</p>
+                    </div>
+            </div>
+        </div>
+    </div>
