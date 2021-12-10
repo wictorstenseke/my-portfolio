d2000dc6662aa8fa454f030364e6fb5934c4faa1
Wictor Stenseke <wictor.stenseke@knowit.se>
2021-12-10 15:06:38 +0100

    changed Knowit to general

--- new_site/includes/pages/index-include.php
+++ new_site/includes/pages/index-include.php
@@ -5,7 +5,7 @@
                 <div class="my-profile">
                     <img class="profile-avatar" src="/img/avatar.jpg">
                     <h3>Wictor Stenseke</h3>
-                    <p>UX Designer at Knowit Solutions</p>
+                    <p>User Experience Designer</p>
                     <p class="bold-text bio-end"><img class="pin" src="/img/pin.svg">Göteborg, Sverige</p>
                     <div class="col-sm-4  col-sm-offset-2 button-set">
                         <a class="btn cta-button-mail" role="button" href="mailto:wictorstenseke@gmail.com">Maila mig&nbsp;&nbsp;&nbsp;<i class="fa fa-paper-plane" aria-hidden="true"></i></a>
